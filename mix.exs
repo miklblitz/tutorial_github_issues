d1327b489fef6607876186c1744393cbe4f01563
Михман <miklblitz@yandex.ru>
2019-02-20 16:53:36 +0400

    doc's mixes updated

--- mix.exs
+++ mix.exs
@@ -9,6 +9,7 @@ defmodule Issuees.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      source_url: "https://github.com/miklblitz/tutorial_github_issues",
       deps: deps()
     ]
   end
@@ -25,7 +26,7 @@ defmodule Issuees.MixProject do
     [
       {:httpoison, "~> 1.4"},
       {:poison, "~>3.1"},
-      {:ex_doc, "~> 0.17.0"},
+      {:ex_doc, "~> 0.19.0"},
       {:earmark, "~> 1.3.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
